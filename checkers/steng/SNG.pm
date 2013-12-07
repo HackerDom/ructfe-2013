b9f2019092189950cfd5117e979dbf20162c980e
Victor Samun <victor.samun@gmail.com>
2013-12-07 11:55:26 +0600

    [steng] pre-release checker

--- checkers/steng/SNG.pm
+++ checkers/steng/SNG.pm
@@ -7,17 +7,51 @@ our @ISA = qw/Exporter/;
 our @EXPORT = qw/generate_simple_sng/;
 our $VERSION = 1;
 
+sub _IHDR {
+	return <<EOH
+IHDR {
+  width: $_[0];
+  height: $_[1];
+  bitdepth: $_[2];
+  $_[3];
+}
+
+EOH
+}
+
 sub generate_simple_sng {
 	my $w = 128 + int rand 64;
 	my $h = 128 + int rand 64;
+	my $d = [8, 16]->[int rand 2];
+	my $a = int rand 2;
+	my $r = _IHDR ($w, $h, $d, 'using color' . ($a ? ' alpha' : '')) . "IMAGE {\n  pixels hex;\n";
 
-	my $r = "IHDR {\nwidth: $w; height: $h; bitdepth: 8;\nusing color;\n}\nIMAGE {\npixels hex\n";
+	my $pat = ($d == 8 ? "%02x" : "%04x") x (3 + $a);
+	$r .= "  " . (join ' ', map { sprintf $pat, int rand (1 << $d), int rand (1 << $d), int rand (1 << $d), int rand (1 << $d) } (1 .. $w)) . "\n" for 1 .. $h;
 
-	for (1 .. $h) {
-		$r .= (join ' ', map { sprintf "%02x%02x%02x", int rand 256, int rand 256, int rand 256 } (1 .. $w)) . "\n";
-	}
+	"$r}";
+}
 
-	$r . '}';
+sub uniq {
+	my %h = map { ($_, '') } @_;
+	keys %h;
+}
+
+sub generate_palette_sng {
+	my $w = 128 + int rand 64;
+	my $h = 128 + int rand 64;
+	my $r = _IHDR ($w, $h, 8, 'using color palette');
+	my $k = 64 + int rand 128;
+
+	my @pal = uniq (map { sprintf "(%3s,%3s,%3s)", int rand 256, int rand 256, int rand 256 } 1 .. $k);
+	$r .= "PLTE {\n" . (join '', map { "  $_\n" } @pal) . "}\n";
+
+	my %hist;
+
+	my $pic = "IMAGE {\n  pixels hex;\n";
+	$pic .= (join '', map { my $x = int rand @pal; ++ $hist {$x}; sprintf "%02x", $x } (1 .. $w)) . "\n" for 1 .. $h;
+
+	"${r}hIST {\n  " . (join (' ', map { $hist {$_} || '0' } (0 .. @pal - 1))) . ";\n}\n$pic}\n";
 }
 
 sub unparse_flag {
--- checkers/steng/steng.checker.pl
+++ checkers/steng/steng.checker.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use constant {
-	DEBUG => 1,
+	DEBUG => 0,
 
 	CHECKER_OK => 101,
 	CHECKER_NOFLAG => 102,
@@ -16,6 +16,7 @@ use constant {
 	CANNOT_PUT_PICTURE => "Can't put picture",
 	FLAG_NOT_FOUND => "Flag not found"
 };
+my @perc = qw/45 35 20/;
 
 use Socket;
 use SNG;
@@ -28,11 +29,13 @@ my %handlers = (
 	'get' => \&get
 );
 
-socket S, PF_INET, SOCK_STREAM, getprotobyname 'tcp';
-$cr = connect S, sockaddr_in $port, inet_aton $ip;
+socket $S, PF_INET, SOCK_STREAM, getprotobyname 'tcp';
+$cr = connect $S, sockaddr_in $port, inet_aton $ip;
 do_exit (CHECKER_DOWN, CONNECTION_ERROR) unless $cr;
 
-vec ($r = '', fileno (S), 1) = 1;
+vec ($v = '', fileno ($S), 1) = 1;
+$| = 1;
+
 $handlers {$mode}->($id, $flag);
 
 sub do_exit {
@@ -42,41 +45,45 @@ sub do_exit {
 
 	print $msg;
 	print STDERR $log;
-	shutdown S, 2;
+	shutdown $S, 2;
 	exit $code;
 }
 
 sub check {
-	send S, "list\n", 0;
-	my @l = split /\s+/, &geta;
-	my $p = int rand @l;
+	send $S, "list\n", 0;
 
-	send S, "getpic $p\n", 0;
-	my $d = &geta;
+	my @list = split /\s+/, &get_all;
+	do_exit (CHECKER_OK) unless @list;
 
-	do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $d =~ /^ERROR/;
+	my $pic = $list [int rand @list];
+
+	send $S, "getpic $pic\n", 0;
+	my $data = &get_all;
+
+	do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $data =~ /^ERROR/ && !($data =~ /^ERROR\(PASSWORD\)/);
 	do_exit (CHECKER_OK);
 }
 
 sub put {
-	my $p = &SNG::generate_simple_sng;
+	my $type = int rand 100;
+	my $pic = ($type < $perc [0] + $perc [1]) ? &SNG::generate_simple_sng : &SNG::generate_palette_sng;
 
-	if (int rand 2) {
-		send S, "putpic $flag\n$p\n", 0;
-		my $d = &geta;
+	if ($type < $perc [0] || $type >= $perc [0] + $perc [1]) {
+		send $S, "putpic $flag\n$pic\n", 0;
+		my $data = &get_all;
 
-		do_exit (CHECKER_MUMBLE, CANNOT_PUT_PICTURE) if $d =~ /^ERROR/;
+		do_exit (CHECKER_MUMBLE, CANNOT_PUT_PICTURE) if $data =~ /^ERROR/;
 
-		print "1 $d";
+		print "1 $data";
 	}
 	else {
 		my $pasw = join '', map { chr (ord ('a') + int rand 26) } 1 .. 8;
-		send S, "putpic $flad $pasw\n$p\n", 0;
-		my $d = &geta;
+		send $S, "putpic $flag $pasw\n$pic\n", 0;
+		my $data = &get_all;
 
-		do_exit (CHECKER_MUMBLE, CANNOT_PUT_PICTURE) if $d =~ /^ERROR/;
+		do_exit (CHECKER_MUMBLE, CANNOT_PUT_PICTURE) if $data =~ /^ERROR/;
 
-		print "2 $pasw:$d";
+		print "2 $pasw:$data";
 	}
 
 	do_exit (CHECKER_OK);
@@ -85,36 +92,38 @@ sub put {
 sub get {
 	my ($id, $flag) = @_;
 
-	my @t = split /\s+/, $id, 2;
-	if ($t[0] == 1) {
-		my @x = split /;/, $t[1];
-		send S, "getpic $x[0]\n", 0;
-		my $d = &geta;
+	my @type = split /\s+/, $id, 2;
+	if ($type [0] == 1) {
+		my @x = split /;/, $type [1];
 
-		do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $d =~ /^ERROR/;
-		do_exit (CHECKER_NOFLAG, FLAG_NOT_FOUND) if SNG::unparse_flag ($d, $x[1]) ne $flag;
+		send $S, "getpic $x[0]\n", 0;
+		my $data = &get_all;
+
+		do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $data =~ /^ERROR/;
+		do_exit (CHECKER_NOFLAG, FLAG_NOT_FOUND) if SNG::unparse_flag ($data, $x[1]) ne $flag;
 	}
 	else {
-		my @x = split /:/, $t[1];
-		send S, "getpic $x[1] $x[0]\n", 0;
-		my $d = &geta;
+		my @x = split /:/, $type [1];
 
-		do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $d =~ /^ERROR/;
-		do_exit (CHECKER_NOFLAG, FLAG_NOT_FOUND) unless $d =~ qr/$flag/;
+		send $S, "getpic $x[1] $x[0]\n", 0;
+		my $data = &get_all;
+
+		do_exit (CHECKER_MUMBLE, CANNOT_GET_PICTURE) if $data =~ /^ERROR/;
+		do_exit (CHECKER_NOFLAG, FLAG_NOT_FOUND) unless $data =~ qr/$flag/;
 	}
 
 	do_exit (CHECKER_OK);
 }
 
-sub geta {
-	my $x = '';
-
-	while (select '' . $r, undef, undef, 0.1) {
-		recv S, ($_ = ''), 1024, 0;
-		return $x unless length;
-		$x .= $_;
+sub get_all {
+	my ($x, $t) = '';
+ 
+	while (select '' . $v, undef, undef, 0.5) {
+		recv $S, ($t = ''), 1024, 0;
+		return $x unless length $t;
+		$x .= $t;
 	}
 
-	$x;
+	return $x;
 }
 
