cc97b22726e6ccfd83d375d8635c3f0a128a1af4
Last G <last_g@hackerdom.ru>
2013-12-10 01:40:46 +0600

    [hammer] checker with working put and almost working get

--- checkers/hammer/build.sbt
+++ checkers/hammer/build.sbt
@@ -1,8 +1,10 @@
 name := "Hammer-checker"
 
-version := "0.1"
+version := "0.3"
 
 libraryDependencies ++= Seq(
 	"org.scalatest" %% "scalatest" % "2.+",
-  "org.seleniumhq.selenium" % "selenium-java" % "2.+"
+  "org.seleniumhq.selenium" % "selenium-java" % "2.+",
+  "commons-codec" % "commons-codec" % "1.8",
+  "org.skife.com.typesafe.config" % "typesafe-config" % "0.3.0"
 )
--- checkers/hammer/src/main/scala/Checker.scala
+++ checkers/hammer/src/main/scala/Checker.scala
@@ -2,9 +2,13 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.scalatest._
 import org.scalatest.concurrent.Eventually._
 import org.scalatest.time.{Seconds, Span, Second}
+import scala.collection.convert.DecorateAsScala
 import selenium._
 import org.openqa.selenium._
 import org.openqa.selenium.chrome.ChromeDriver
+import org.apache.commons.codec.digest.DigestUtils
+import org.scalatest.OptionValues._
+import com.typesafe.config.ConfigFactory
 
 object CheckUsage extends Exception
 
@@ -15,7 +19,13 @@ object Checker {
   val DOWN = 104
   val ERROR = 110
 
-  val port = 9000
+  val conf = ConfigFactory.load
+
+  val port = conf.getInt("hammer.port")
+  val salt = conf.getString("hammer.salt")
+
+  val adminLogin = conf.getString("hammer.admin.login")
+  val adminName  = conf.getString("hammer.admin.name")
 
   def do_check(args: Array[String]):Unit = {
     if (args.length < 2) {
@@ -35,22 +45,31 @@ object Checker {
     }
     catch {
       case CheckUsage|(_:ArrayIndexOutOfBoundsException) => {
-        println("Please check usage of this checker")
-        println("./checker {mode} {host} [{id} {flag}]")
+
+        System.err.println("Please check usage of this checker")
+        System.err.println("./checker {mode} {host} [{id} {flag}]")
         System.exit(ERROR)
       }
       case error:Throwable => {
-        println("Unknown error in checker, please talk to  orgs")
+        System.err.println("Unknown error in checker, please talk to  orgs")
         error.printStackTrace(System.err)
         System.exit(ERROR)
       }
     }
   }
+
+  def userLogin(id:String) = id
+  def userPass(id:String)  = DigestUtils.md5Hex(Checker.salt+id)
+  def userName(id:String)  = id
 }
 
-class Checker (host:String, port:Int) extends Firefox with Matchers  {
+class Checker (host:String, port:Int) extends FlatSpec with Firefox with Matchers with DecorateAsScala   {
 
   def baseUrl = s"http://$host:$port/"
+
+  def adminName = Checker.adminName
+  def adminLogin = Checker.adminLogin
+  lazy val adminPassword = DigestUtils.md5Hex(Checker.salt + host)
 
   def doAction(action:String, args:Array[String]) = {
     try {
@@ -64,6 +83,7 @@ class Checker (host:String, port:Int) extends Firefox with Matchers  {
     catch {
       case e:exceptions.TestFailedException => {
         // Rethrowing original exception if any
+        System.err.println("Repacking exception")
         throw e.cause.getOrElse(e)
       }
     }
@@ -72,18 +92,36 @@ class Checker (host:String, port:Int) extends Firefox with Matchers  {
     }
   }
 
+  def goBase = if (currentUrl != baseUrl) go to baseUrl
+  def goSite = if (currentUrl.contains(baseUrl)) go to baseUrl
+
+
+
+  def goToMessage(msgId: Int) = {
+    goSite
+    click on partialLinkText("Austropat")
+    val links = findAll(cssSelector ("warp-td-mark")).filter { elem =>
+      elem.attribute("id").exists({ id =>
+        s"warp-td-mark-$msgId".equalsIgnoreCase(id)
+      })
+    }
+    clickOn(links.toSeq(0))
+}
+
   def checkLoggedOn() = find(partialLinkText("Logout")).isDefined
 
   def doLoginOrNothing(login: String, password: String) = {
-    go to baseUrl
-    find(partialLinkText("Logout")).fold
-      {}
-      {_ => doLogin(login, password)}
+    goBase
+    find(partialLinkText("Logout")).map { _=> doLogin(login, password)}
+  }
 
+  def doLogout = {
+    goBase
+    find(partialLinkText("Logout")).map(click on _)
   }
 
   def doRegister(login: String, password: String, name: String) = {
-    go to baseUrl
+    goBase
     click on linkText("Registration")
 
     val registrationUrl = currentUrl
@@ -95,32 +133,144 @@ class Checker (host:String, port:Int) extends Firefox with Matchers  {
     click on "password_confirm"
     enter(password)
     click on "name"
-    enter("name")
+    enter(name)
 
     submit()
 
     eventually {
-      Thread.sleep(5000)
+      Thread.sleep(100)
       currentUrl shouldNot be(registrationUrl)
       currentUrl shouldNot include("register")
     }
   }
 
   def doLogin(login: String, password: String) = {
-    go to baseUrl
+    goBase
     click on "login"
     enter (login)
     click on "password"
     enter (password)
     submit()
+
+    eventually {
+      Thread.sleep(500)
+      currentUrl shouldNot include("register")
+      find(partialLinkText("Logout")) shouldNot be(None)
+    }
+  }
+
+  def doLoginOrRegister(login:String, password:String):Any = doLoginOrRegister(login, password, login)
+
+  def doLoginOrRegister(login:String, password:String, name:String):Any = {
+    goBase
+    doLogout
+
+    try {
+      doLogin(login, password)
+    }
+    catch { case _:exceptions.TestFailedException =>
+      doRegister(login, password, name)
+    }
+  }
+
+  def doCreate(pub:String, priv:Option[String]) = {
+    goSite
+    click on partialLinkText("Create")
+
+    // Cheat
+    eventually {
+//      Thread.sleep(100)
+      click on(cssSelector("a[data-wysihtml5-action=change_view]"))
+    }(PatienceConfig(timeout = Span(10, Seconds)))
+    click on("public")
+    enter(pub)
+
+//    executeScript(s"return $$('#public-text').value='$pub';")
+//    execute_script()
+    priv.map { secret =>
+      click on "private"
+      enter(secret)
+    }
+    submit()
+
+
+    eventually {
+      Thread.sleep(100)
+      if(priv.isDefined) {
+        find("warp-decrypt").value.text should be(pub)
+        find("warp-secret").value.text should be(priv.get)
+      } else {
+        find("warp-public").value.text should be(pub)
+      }
+    }
+
+    Integer.parseInt(find("warp-id").get.text)
+  }
+
+  def doSend(msg: Int, name: String) = {
+    if(!pageTitle.equalsIgnoreCase(s"Message $msg")) {
+      goToMessage(msg)
+    }
+
+    val option = find(cssSelector("#warp-sender-select option[data-name=\"" + name + "\"]"))
+
+    option should not be('empty)
+
+    singleSel("warp-sender-select").value = option.get.attribute("value").get
+    click on partialLinkText("Send")
+
+    eventually {
+      find(cssSelector(".alert-success")) should be('defined)
+    }(PatienceConfig(timeout = Span(10, Seconds)))
   }
 
   def put(id: String, flag: String) = {
+    goBase
+    doLogout
 
+    val login = Checker.userLogin(id)
+    val password = Checker.userPass(id)
+    val name = Checker.userName(id)
+
+    doRegister(login, password, name)
+    doLogout
+
+    doLoginOrRegister(adminLogin, adminPassword, adminName)
+    val msgid = doCreate(flag, Some(DigestUtils.sha256Hex(Checker.salt + id).substring(0,24)))
+    doSend(msgid, name)
+  }
+
+  def doCheckMessageFrom(name: String, flag: String) = {
+    click on partialLinkText("Incoming")
+    click on partialLinkText("All")
+
+    val messagesFrom = findAll(cssSelector(".warp-td-author")).toArray//.filter(_.text == name)
+
+
+    println(messagesFrom.map(_.text).mkString(" "))
+
+    messagesFrom.length should be > (0)
+
+    messagesFrom.map({ message =>
+      val id = Integer.parseInt(message.attribute("data-id").get)
+      find(cssSelector(s"#warp-td-id-$id a")).get.attribute("href").get
+    }).exists({ link =>
+      go to link
+      find("warp-decrypt").map(_.text == flag).getOrElse(false)
+    }) should be(true)
   }
 
   def get(id: String, flag: String) = {
+    goBase
+    doLogout
 
+    val login = Checker.userLogin(id)
+    val password = Checker.userPass(id)
+    val name = Checker.userName(id)
+
+    doLogin(login, password)
+
+    doCheckMessageFrom(adminName, flag)
   }
 
   def check() = {
@@ -130,7 +280,7 @@ class Checker (host:String, port:Int) extends Firefox with Matchers  {
   def test() = {
    try {
      val host = "http://google.com/"
-     go to host
+     goBase
      click on "q"
      pressKeys("Hooray!")
      submit()
