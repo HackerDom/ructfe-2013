4cb22953e4af5f81c943120de637ccd31349e101
Bogdan Melnik <ld@hackerdom.ru>
2013-12-11 01:02:10 +0600

    [nsaless] Remove legacy

--- services/nsaless/routes/crypto.js
+++ services/nsaless/routes/crypto.js
@@ -78,27 +78,3 @@ exports.encryptTweet = function(user, tweet) {
     tweet.tweet = exports.encryptWithUser(user, bignum.fromBuffer(message));
     return tweet;
 }
-
-/*
-exports.prime = function(bits) {
-    return bignum.prime(bits);
-}
-
-var RSA = function() {
-    this.p = exports.prime(512);
-    this.q = exports.prime(512);
-
-    this.e = bignum(7);
-    this.n = this.p.mul(this.q);
-    this.phi = this.p.sub(1).mul(this.q.sub(1));
-    this.d = this.e.invertm(this.phi);
-}
-
-exports.RSA = RSA;
-exports.RSA.prototype.encrypt = function(num) {
-    return bignum(num).powm(this.e, this.n);
-}
-exports.RSA.prototype.decrypt = function(num) {
-    return bignum(num).powm(this.d, this.n);
-}
-*/
