08acc6973ab4bbff2b733ff5b363254fab7f2548
dimmo <dimmoborgir@gmail.com>
2013-12-14 01:25:10 +0600

    fixed typo

--- checksystem/db/test-teams-add.sql
+++ checksystem/db/test-teams-add.sql
@@ -1,7 +1,7 @@
 -- Test teams for RuCTFE-2013
 
-INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 1, 'Team 1' , '10.22.0.6/32',  '10.22.0.6' , t);
-INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 2, 'Team 2' , '10.22.0.54/32', '10.22.0.54', t);
-INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 3, 'Team 3' , '10.22.0.46/32', '10.22.0.46', t);
-INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 4, 'Team 4' , '10.22.0.70/32', '10.22.0.70', t);
-INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 5, 'Team 5' , '10.22.0.74/32', '10.22.0.74', t);
+INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 1, 'Team 1' , '10.22.0.6/32',  '10.22.0.6' , true);
+INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 2, 'Team 2' , '10.22.0.54/32', '10.22.0.54', true);
+INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 3, 'Team 3' , '10.22.0.46/32', '10.22.0.46', true);
+INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 4, 'Team 4' , '10.22.0.70/32', '10.22.0.70', true);
+INSERT INTO TEAMS (id,name,network,vuln_box,enabled) VALUES ( 5, 'Team 5' , '10.22.0.74/32', '10.22.0.74', true);
