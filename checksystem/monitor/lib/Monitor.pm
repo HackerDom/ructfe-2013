23cf24225bbfb326f13c57da88dbdc248f8bea91
Andrey Khozov <avkhozov@gmail.com>
2013-12-14 02:52:47 +0600

    [monitor] Initial release

---
+++ checksystem/monitor/lib/Monitor.pm
@@ -0,0 +1,26 @@
+package Monitor;
+use Mojo::Base 'Mojolicious';
+
+use DBI;
+
+sub startup {
+  my $self = shift;
+
+  my $mode = $self->mode;
+  $self->plugin('Config', file => "monitor.$mode.conf");
+
+  my $r = $self->routes;
+  $r->get('/')->to('main#index');
+  $r->get('/flags')->to('main#flags');
+
+  $self->helper(
+  db => sub {
+    DBI->connect_cached(
+      $self->config->{db}->{source},
+      $self->config->{db}->{user},
+      $self->config->{db}->{pass},
+      {RaiseError => 1, AutoCommit => 1, pg_enable_utf8 => 1});
+  });
+}
+
+1;
